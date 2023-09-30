938f31e0cdd5fffc62653364041f8fb9b717f036
haris ardian <harisfirman387@gmail.com>
2023-09-30 07:59:15 +0700

    uri

--- main.go
+++ main.go
@@ -2,8 +2,8 @@ package main
 
 import (
 	"go-todolist-web/config"
-	"go-todolist-web/controller/listcontoller"
-	"go-todolist-web/controller/homecontroller"
+	"go-todolist-web/controller/completecontroller"
+	listcontroller "go-todolist-web/controller/listcontoller"
 	"log"
 	"net/http"
 )
@@ -12,14 +12,14 @@ func main() {
 	config.ConnectDb()
 
 	//homepage
-	http.HandleFunc("/", homecontroller.Welcome)
+	http.HandleFunc("/", listcontroller.Index)
+	http.HandleFunc("/complete", completecontroller.Home)
 
-	//categories
-	http.HandleFunc("/categories", listcontroller.Index)
-	http.HandleFunc("/categories/add", listcontroller.Add)
-	http.HandleFunc("/categories/edit", listcontroller.Edit)
-	http.HandleFunc("/categories/delete", listcontroller.Delete)
+	//list
+	http.HandleFunc("/add", listcontroller.Add)
+	http.HandleFunc("/edit", listcontroller.Edit)
+	http.HandleFunc("/delete", listcontroller.Delete)
 
-	log.Print("Server berjalan (8080)")
-	http.ListenAndServe(":8080", nil)
+	log.Print("Server berjalan (3000)")
+	http.ListenAndServe(":3000", nil)
 }
