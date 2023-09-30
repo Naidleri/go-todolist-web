ee7621cc32e6476252735c3d3e5c4958af793f02
haris ardian <harisfirman387@gmail.com>
2023-09-30 07:58:52 +0700

    db config

--- config/database.go
+++ config/database.go
@@ -8,12 +8,12 @@ import (
 	_ "github.com/go-sql-driver/mysql"
 )
 
-	var DB *sql.DB
+var DB *sql.DB
 
-func ConnectDb () {
-	db, err := sql.Open("mysql", "root:root@/todolist_go")
+func ConnectDb() {
+	db, err := sql.Open("mysql", "root:root@tcp(localhost:3308)/todolist_go?parseTime=true")
 	if err != nil {
-	panic(err)
+		panic(err)
 	}
 
 	log.Println("Db connected")
