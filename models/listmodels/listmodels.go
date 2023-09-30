07c75dcd8d56a3369d62611034a027456ab2d02c
haris ardian <harisfirman387@gmail.com>
2023-09-30 07:58:40 +0700

    Config and Create

---
+++ models/listmodels/listmodels.go
@@ -0,0 +1,43 @@
+package listmodels
+
+import (
+	"go-todolist-web/config"
+	"go-todolist-web/entitites"
+)
+
+func GetAll() []entitites.List {
+	rows, err := config.DB.Query("select * from todolist")
+	if err != nil {
+		panic(err)
+	}
+
+	defer rows.Close()
+
+	var categories []entitites.List
+
+	for rows.Next() {
+		var list entitites.List
+		if err := rows.Scan(&list.Id, &list.Task, &list.Deadline, &list.Completed); err != nil {
+			panic(err)
+		}
+
+		categories = append(categories, list)
+	}
+	return categories
+
+}
+
+func Create(todolist entitites.List) bool {
+	result, err := config.DB.Exec("INSERT INTO todolist (task,deadline,completed) VALUE (?,?,?)", todolist.Task, todolist.Deadline, todolist.Completed)
+
+	if err != nil {
+		panic(err)
+	}
+
+	lastInsertId, err := result.LastInsertId()
+	if err != nil {
+		panic(err)
+	}
+
+	return lastInsertId > 0
+}
